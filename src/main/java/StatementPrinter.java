702df5b81cfb14cdaabfe72800f4d06cee9a3283
ayoubhatmi <ayoub.hatmi18@gmail.com>
2023-10-10 17:12:48 +0200

    intial commit

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -3,30 +3,34 @@ import java.util.*;
 
 public class StatementPrinter {
 
-  public String print(Invoice invoice, HashMap<String, Play> plays) {
-    int totalAmount = 0;
-    int volumeCredits = 0;
-    String result = String.format("Statement for %s\n", invoice.customer);
+  public static final String TRAGEDY = "tragedy";
+  public static final String COMEDY = "comedy";
 
+  public String print(Invoice invoice, HashMap<String, Play> plays) {
+    double totalAmount = 0;
+    int volumeCredits = 0;
+    StringBuilder result = new StringBuilder();
     NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+
+    result.append(String.format("Statement for %s\n", invoice.customer));
 
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
-      int thisAmount = 0;
+      double thisAmount = 0;
 
       switch (play.type) {
-        case "tragedy":
-          thisAmount = 40000;
+        case TRAGEDY:
+          thisAmount = 400.0;
           if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30);
+            thisAmount += 10.00 * (perf.audience - 30);
           }
           break;
-        case "comedy":
-          thisAmount = 30000;
+        case COMEDY:
+          thisAmount = 300.00;
           if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
+            thisAmount += 100.00 + 5.00 * (perf.audience - 20);
           }
-          thisAmount += 300 * perf.audience;
+          thisAmount += 3.00 * perf.audience;
           break;
         default:
           throw new Error("unknown type: ${play.type}");
@@ -35,15 +39,24 @@ public class StatementPrinter {
       // add volume credits
       volumeCredits += Math.max(perf.audience - 30, 0);
       // add extra credit for every ten comedy attendees
-      if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+      if (COMEDY.equals(play.type)) volumeCredits +=
+        Math.floor(perf.audience / 5);
 
-      // print line for this order
-      result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
+      // append line for this order
+      result.append(
+        String.format(
+          "  %s: %s (%s seats)\n",
+          play.name,
+          frmt.format(thisAmount),
+          perf.audience
+        )
+      );
       totalAmount += thisAmount;
     }
-    result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
-    result += String.format("You earned %s credits\n", volumeCredits);
-    return result;
+    result.append(
+      String.format("Amount owed is %s\n", frmt.format(totalAmount))
+    );
+    result.append(String.format("You earned %s credits\n", volumeCredits));
+    return result.toString();
   }
-
 }
